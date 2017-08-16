ab93dbc1c2bd263b8737cc1725028eddcfe6aff1
Vanesa <mail@vanesaortiz.com>
2017-08-15 23:40:43 -0700

    refactor solution to show double pairs, add unittest

--- findpairs.py
+++ findpairs.py
@@ -4,21 +4,49 @@
 
 # (5, 0) (2, 3) (1, 4)
 
+import unittest
+
 def findpairs(array, k):
     
     tracker = {}
+    result = []
     
     for i, n in enumerate(array):
+
         if n * 2 == k:
-            print n, n
-
-        if n in tracker.keys():
-            i2 = tracker[n]
-            
-            print array[i2], n
-
-        n2 = k - n
-        tracker[n2] = i
+            result.append((n, n))
+            continue
         
+        n2 = k - n
+        
+        if n in tracker.keys():
+            
+            for i2 in tracker[n]:
+                result.append((array[i2], n))
+        
+        if n2 in tracker.keys():
+            tracker[n2].append(i)
+            continue
+        
+        tracker[n2] = [i]
+        
+    return result
 
-findpairs([5, 6, 1, 2, 9, 0, 3, 4], 5)
+class Test(unittest.TestCase):
+    data = [(
+        [[5, 6, 1, 2, 9, 0, 3, 4], 5], 
+        [(5, 0),(2, 3),(1, 4)]
+        ),
+    (
+        [[1, 6, 9, 5, 3, 6, 4, 8, 0, 2, 2, 4], 6], 
+        [(1, 5), (3, 3), (6, 0), (6, 0), (4, 2), (4, 2), (2, 4), (2, 4)]
+        )
+    ]
+
+    def test_findpairs(self):
+        for [[array, num], expected] in self.data:
+            actual = findpairs(array, num)
+            self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
